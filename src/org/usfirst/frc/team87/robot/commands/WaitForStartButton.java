32d8fe67e65a16ea6f99bbdc1341ca641e23fbde
Andrew Wiik <andrew.wiik@vtsd.com>
2017-03-16 16:10:37 -0700

    forgot to remove redundant gamepad initilializer

--- src/org/usfirst/frc/team87/robot/commands/WaitForStartButton.java
+++ src/org/usfirst/frc/team87/robot/commands/WaitForStartButton.java
@@ -3,23 +3,20 @@ package org.usfirst.frc.team87.robot.commands;
 import org.usfirst.frc.team87.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class WaitForStartButton extends Command {
 
 	
 	public boolean isFinished;
-	public static Joystick gameController;
 	
 	public WaitForStartButton() {
 		isFinished = false;
-		gameController = new Joystick(RobotMap.GAMEPAD);
 		DriverStation.reportWarning("Please press the 'Start' button on the gamepad to continue", false);
 	}
 	
 	protected void execute() {
-		if (gameController.getRawButton(RobotMap.REVERSE)) {
+		if (RobotMap.gamepad.getRawButton(RobotMap.REVERSE)) {
 			isFinished = true;
 		}
     }
